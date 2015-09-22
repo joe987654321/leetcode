2fe93179e97a31b908af6eebe7cd7561071b3d78
Joe Hsu <joe321@yahoo-inc.com>
2015-09-22 10:47:30 +0800

    implement longestConsecutiveSequence

--- aaa/src/com/yahoo/nevec/egs/product/consumer/LongestConsecutiveSequence.java
+++ aaa/src/com/yahoo/nevec/egs/product/consumer/LongestConsecutiveSequence.java
@@ -1,14 +1,46 @@
 package com.yahoo.nevec.egs.product.consumer;
 
+import java.util.HashSet;
+
 public class LongestConsecutiveSequence {
     public static class Solution {
         public int longestConsecutive(int[] nums) {
+            HashSet<Integer> hs = new HashSet<Integer>();
+            for (int v: nums) {
+                hs.add(v);
+            }
 
+            int maxLength = 0 ;
+            for (int v: nums) {
+                if (hs.contains(v)) {
+                    hs.remove(v);
+                    int upper = v+1;
+                    while (hs.contains(upper)) {
+                        hs.remove(upper);
+              //          System.out.println("find upper " + upper);
+                        upper++;
+                    }
+                    upper--;
+
+                    int lower = v-1;
+                    while (hs.contains(lower)) {
+                        hs.remove(lower);
+                    //    System.out.println("find lower " + lower);
+                        lower--;
+                    }
+                    lower++;
+
+                    maxLength = Math.max(maxLength, upper-lower+1);
+                }
+            }
+
+            return maxLength;
         }
     }
 
     public static void main(String[] args) {
-        scramableString.Solution s = new scramableString.Solution();
-        System.out.println(s.isScramble("abcd", "bdac"));
+        Solution s = new Solution();
+        int [] a = {100, 2, 4, 200, 3, 1};
+        System.out.println(s.longestConsecutive(a));
     }
 }
