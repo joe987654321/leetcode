475b9e3534d0b22784cf8cd89e182b2316e8be3a
Joe Hsu <joe321@yahoo-inc.com>
2015-09-24 17:20:56 +0800

    add AddTwoNumbers.java, init mergeKList.java (not implement yet)

---
+++ aaa/src/com/yahoo/nevec/egs/product/consumer/mergeKLists.java
@@ -0,0 +1,19 @@
+package com.yahoo.nevec.egs.product.consumer;
+
+public class mergeKLists {
+
+
+    //Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+
+    public class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            return null;
+        }
+    }
+}
