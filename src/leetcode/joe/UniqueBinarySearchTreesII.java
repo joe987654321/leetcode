0b5599b46df2292f58686ec61bbc150b21ecde6e
Joe Hsu <joe321@yahoo-inc.com>
2018-03-16 18:37:49 +0800

    uniqueBinarySearchTreesII

--- src/leetcode/joe/UniqueBinarySearchTreesII.java
+++ src/leetcode/joe/UniqueBinarySearchTreesII.java
@@ -1,5 +1,8 @@
 package leetcode.joe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import leetcode.treenode.TreeNode;
@@ -7,10 +10,45 @@ import leetcode.treenode.TreeNode;
 public class UniqueBinarySearchTreesII {
     public static void main(String[] args) {
 
+        UniqueBinarySearchTreesII uniqueBinarySearchTreesII = new UniqueBinarySearchTreesII();
+        List<TreeNode> treeNodes = uniqueBinarySearchTreesII.generateTrees(3);
 
+        for (int i = 0; i < treeNodes.size(); i++) {
+            System.out.println("root is " + treeNodes.get(i).val);
+        }
     }
 
     public List<TreeNode> generateTrees(int n) {
-        return null;
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return _generateTrees(1, n);
+    }
+
+    private List<TreeNode> _generateTrees(int start, int end) {
+        if (end < start) {
+            return Collections.singletonList(null);
+        }
+        if (start == end) {
+            return Collections.singletonList(new TreeNode(start));
+        }
+
+        List<TreeNode> ret = new ArrayList<>();
+
+        for (int i = start; i <= end ; i++) {
+            List<TreeNode> leftTreeNodes = _generateTrees(start, i-1);
+            List<TreeNode> rightTreeNodes = _generateTrees(i+1 , end);
+
+            for (int j = 0; j < leftTreeNodes.size(); j++) {
+                for (int k = 0; k < rightTreeNodes.size(); k++) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = leftTreeNodes.get(j);
+                    root.right = rightTreeNodes.get(k);
+
+                    ret.add(root);
+                }
+            }
+        }
+        return ret;
     }
 }
