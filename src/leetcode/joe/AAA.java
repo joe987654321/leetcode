1f87cacd5922d5892087995cb3c6c36f968649c1
Joe Hsu <joe321@yahoo-inc.com>
2019-02-08 17:55:50 +0800

    binary search

--- src/leetcode/joe/AAA.java
+++ src/leetcode/joe/AAA.java
@@ -6,17 +6,14 @@ import java.util.*;
 
 public class AAA {
     public static void main(String[] args) {
-        Map<List<Integer>, Integer> map = new HashMap<>();
-        List<Integer> a1 = new ArrayList<>();
-        a1.add(1);
-        map.put(a1,1);
+        Integer [] a = {6,5,4,2,1};
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i + " : " + Arrays.binarySearch(a, 0, 3, i, Comparator.reverseOrder()));
+        }
 
-        System.out.println(map.get(a1));
-
-        List<Integer> a2 = new LinkedList<>();
-        a2.add(1);
-
-        System.out.println(map.get(a2));
-
+        List<Integer> b = Arrays.asList(a);
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i + " : " + Collections.binarySearch(b, i));
+        }
     }
 }
