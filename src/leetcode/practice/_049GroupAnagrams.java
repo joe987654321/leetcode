604707c9dae115efb6b8d9999320b35644b94f97
Joe Hsu <joejoe321321@gmail.com>
2019-02-10 15:43:21 +0800

    049 050 practice

---
+++ src/leetcode/practice/_049GroupAnagrams.java
@@ -0,0 +1,38 @@
+package leetcode.practice;
+
+import java.util.*;
+
+public class _049GroupAnagrams {
+    public static void main(String[] args) {
+        _049GroupAnagrams groupAnagrams = new _049GroupAnagrams();
+        List<List<String>> lists = groupAnagrams.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat" });
+        System.out.println(lists);
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, Integer> map = new HashMap<>();
+        List<List<String>> ret = new ArrayList<>();
+        int index = 0;
+        for (int i = 0; i < strs.length; i++) {
+            int[] a = new int[26];
+            for (int j = 0; j < strs[i].length(); j++) {
+                a[strs[i].charAt(j)-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 26; j++) {
+                sb.append(a[j]).append('#');
+            }
+            Integer tryIndex = map.get(sb.toString());
+            if (tryIndex == null) {
+                map.put(sb.toString(), index);
+                List<String> tmp = new ArrayList<>();
+                tmp.add(strs[i]);
+                ret.add(tmp);
+                index++;
+            } else {
+                ret.get(tryIndex).add(strs[i]);
+            }
+        }
+        return ret;
+    }
+}
---
+++ src/leetcode/practice/_050PowXN.java
@@ -0,0 +1,56 @@
+package leetcode.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _050PowXN {
+    public static void main(String[] args) {
+        _050PowXN powXN = new _050PowXN();
+        System.out.println(powXN.myPow(2.0, -3));
+    }
+
+    double myPow(double x, int n) {
+        double res = 1.0;
+        for (int i = n; i != 0; i /= 2) {
+            if (i % 2 != 0) res *= x;
+            x *= x;
+        }
+        return n < 0 ? 1 / res : res;
+    }
+
+    //too long
+    public double myPow2(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        long nn = n;
+        boolean sign = true;
+        if (nn <0) {
+            nn = -nn;
+            sign = false;
+        }
+        List<Double> list = new ArrayList<>();
+        long count = 1;
+        while (count <= nn) {
+            list.add(x);
+            x *= x;
+            count*=2;
+        }
+        count/=2;
+        int indexFromTail = 0;
+        double ret = 1;
+        while(nn != 0) {
+            if (nn >= count) {
+                ret *= list.get(list.size()-1-indexFromTail);
+                nn-=count;
+            }
+            count/=2;
+            indexFromTail++;
+        }
+        return sign ? ret : 1/ret;
+    }
+
+}
