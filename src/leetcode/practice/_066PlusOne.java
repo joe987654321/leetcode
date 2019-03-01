8e0c3237aecc7884d5d0117d7d9d1b96e4021f63
Joe Hsu <joejoe321321@gmail.com>
2019-03-01 22:47:22 +0800

    066 067 068 practice

---
+++ src/leetcode/practice/_066PlusOne.java
@@ -0,0 +1,29 @@
+package leetcode.practice;
+
+import java.util.Arrays;
+
+public class _066PlusOne {
+
+    public static void main(String[] args) {
+
+        _066PlusOne plusOne = new _066PlusOne();
+
+        int[] ret = plusOne.plusOne(new int[]{9});
+        System.out.println(Arrays.toString(ret));
+    }
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >= 0; i--) {
+            digits[i]++;
+            if (digits[i] == 10) {
+                digits[i] = 0;
+            } else break;
+        }
+        if (digits[0] == 0) {
+            int[] ret = new int[digits.length+1];
+            ret[0] = 1;
+            return ret;
+        }
+        return digits;
+    }
+}
---
+++ src/leetcode/practice/_067AddBinary.java
@@ -0,0 +1,25 @@
+package leetcode.practice;
+
+public class _067AddBinary {
+    public static void main(String[] args) {
+        _067AddBinary addBinary = new _067AddBinary();
+        String s = addBinary.addBinary("1010", "101");
+        System.out.println(s);
+    }
+
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        for (int i = 0; i < a.length() || i < b.length(); i++) {
+            if (i<a.length()) carry += a.charAt(a.length()-1-i)-'0';
+            if (i<b.length()) carry += b.charAt(b.length()-1-i)-'0';
+            int res = carry % 2;
+            carry /= 2;
+            sb.insert(0, res);
+        }
+        if (carry != 0) {
+            sb.insert(0, carry);
+        }
+        return sb.toString();
+    }
+}
