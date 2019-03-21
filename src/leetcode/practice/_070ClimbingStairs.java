0b6b88bfaedae510bec04f4035ff405f0e14d8ed
Joe Hsu <joejoe321321@gmail.com>
2019-03-22 00:10:01 +0800

    069 070 071 practice

---
+++ src/leetcode/practice/_070ClimbingStairs.java
@@ -0,0 +1,21 @@
+package leetcode.practice;
+
+public class _070ClimbingStairs {
+    public static void main(String[] args) {
+        _070ClimbingStairs climbingStairs = new _070ClimbingStairs();
+        int ret = climbingStairs.climbStairs(4);
+        System.out.println(ret);
+    }
+
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        int x = 1, y = 2;
+        n-=2;
+        while (n != 0) {
+            y = x + y;
+            x = y - x;
+            n--;
+        }
+        return y;
+    }
+}
