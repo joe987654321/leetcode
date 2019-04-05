ba1d43630f0934bcc7a1a783b40f934d78347910
Joe Hsu <joe321@yahoo-inc.com>
2019-04-05 21:51:54 +0800

    092 093 094 095 096 097 practice

---
+++ src/leetcode/practice/_093RestoreIPAddresses.java
@@ -0,0 +1,41 @@
+package leetcode.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _093RestoreIPAddresses {
+    public static void main(String[] args) {
+        _093RestoreIPAddresses restoreIPAddresses = new _093RestoreIPAddresses();
+        List<String> strings = restoreIPAddresses.restoreIpAddresses("25525511135");
+        System.out.println(strings);
+    }
+
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ret = new ArrayList<>();
+        if (s == null || s.length() < 4 || s.length() > 12) return ret;
+        _restore(ret, s , new ArrayList<>(), 0);
+        return ret;
+    }
+
+    private void _restore(List<String> ret, String s, List<String> now, int idx) {
+        if (now.size() == 4) {
+            if (idx == s.length()) {
+                String sb = now.get(0) + '.' + now.get(1) + '.' + now.get(2) + '.' + now.get(3);
+                ret.add(sb);
+            }
+            return;
+        }
+        int left = 4 - now.size();
+        if (s.length() - idx < left || s.length() - idx > 3 * left) return;
+        for (int i = 1; i <= 3; i++) {
+            if (idx + i > s.length()) return;
+            if (i != 1 && s.charAt(idx) == '0') return;
+            int ip = Integer.parseInt(s.substring(idx, idx+i));
+            if (ip <= 255) {
+                now.add(String.valueOf(ip));
+                _restore(ret, s, now, idx+i);
+                now.remove(now.size()-1);
+            }
+        }
+    }
+}
---
+++ src/leetcode/practice/_096UniqueBinarySearchTrees.java
@@ -0,0 +1,19 @@
+package leetcode.practice;
+
+public class _096UniqueBinarySearchTrees {
+    public static void main(String[] args) {
+        _096UniqueBinarySearchTrees uniqueBinarySearchTrees = new _096UniqueBinarySearchTrees();
+        int i = uniqueBinarySearchTrees.numTrees(4);
+        System.out.println(i);
+    }
+
+    public int numTrees(int n) {
+        //formula: C(2n, n) - C(2n, n+1) = C(2n,n) / (n+1)
+        long now = 1;
+        for (int i = 0; i < n; i++) {
+            now *= 2*n-i;
+            now /= i+1;
+        }
+        return (int)(now/(n+1));
+    }
+}
