40eac38f28821571113cf826dd336a99139e1335
Joe Hsu <joejoe321321@gmail.com>
2019-06-19 23:34:35 +0800

    aline

--- src/leetcode/joe/SelfCrossing.java
+++ src/leetcode/joe/SelfCrossing.java
@@ -6,7 +6,7 @@ public class SelfCrossing {
     public static void main(String[] args) {
         SelfCrossing selfCrossing = new SelfCrossing();
 //        int [] a = {1,2,3,4,3,1,2,4};
-        int [] a = {1,2,3,4,4,3,1};
+        int[] a = {1, 2, 3, 4, 4, 3, 1};
         boolean selfCrossing1 = selfCrossing.isSelfCrossing(a);
         System.out.println(selfCrossing1);
     }
@@ -14,17 +14,17 @@ public class SelfCrossing {
     //didn't consider two line are overlapping, ex: [2,1,3,1,2]
     //and axis[X|Y] is not needed, x itself can give full information
     public boolean isSelfCrossing(int[] x) {
-        if (x == null ||x.length <=3) return false;
-        int [] axisX = {0   , -x[1], -x[1]    ,0,0,0};
-        int [] axisY = {x[0],  x[0], x[0]-x[2],0,0,0};
+        if (x == null || x.length <= 3) return false;
+        int[] axisX = {0, -x[1], -x[1], 0, 0, 0};
+        int[] axisY = {x[0], x[0], x[0] - x[2], 0, 0, 0};
         int nowx = -x[1];
-        int nowy = x[0]-x[2];
+        int nowy = x[0] - x[2];
 
-        for (int i=3; i<x.length; i++) {
-            if (i%4==0) nowy+=x[i];
-            if (i%4==1) nowx-=x[i];
-            if (i%4==2) nowy-=x[i];
-            if (i%4==3) nowx+=x[i];
+        for (int i = 3; i < x.length; i++) {
+            if (i % 4 == 0) nowy += x[i];
+            if (i % 4 == 1) nowx -= x[i];
+            if (i % 4 == 2) nowy -= x[i];
+            if (i % 4 == 3) nowx += x[i];
 
 //            System.out.println(Arrays.toString(axisX));
 //            System.out.println(Arrays.toString(axisY));
@@ -35,20 +35,20 @@ public class SelfCrossing {
 //            System.out.println(between(nowy, axisY[(i+3)%6], axisY[(i+2)%6]));
 //            System.out.println(between(axisX[(i+3)%6],nowx,axisX[(i+5)%6]));
 
-            if (i%2==1) {
-                if (between(nowy, axisY[(i+3)%6], axisY[(i+2)%6]) && between(axisX[(i+3)%6],nowx,axisX[(i+5)%6])
-                    || between(nowy, axisY[(i+1)%6], axisY[i%6]) && between(axisX[i%6],nowx,axisX[(i+5)%6])) {
+            if (i % 2 == 1) {
+                if (between(nowy, axisY[(i + 3) % 6], axisY[(i + 2) % 6]) && between(axisX[(i + 3) % 6], nowx, axisX[(i + 5) % 6])
+                        || between(nowy, axisY[(i + 1) % 6], axisY[i % 6]) && between(axisX[i % 6], nowx, axisX[(i + 5) % 6])) {
                     return true;
                 }
             } else {
-                if (between(nowx, axisX[(i+3)%6], axisX[(i+2)%6]) && between(axisY[(i+3)%6],nowy,axisY[(i+5)%6])
-                        || between(nowx, axisX[(i+1)%6], axisX[i%6]) && between(axisY[i%6],nowy,axisY[(i+5)%6])) {
+                if (between(nowx, axisX[(i + 3) % 6], axisX[(i + 2) % 6]) && between(axisY[(i + 3) % 6], nowy, axisY[(i + 5) % 6])
+                        || between(nowx, axisX[(i + 1) % 6], axisX[i % 6]) && between(axisY[i % 6], nowy, axisY[(i + 5) % 6])) {
                     return true;
                 }
             }
 
-            axisX[i%6]=nowx;
-            axisY[i%6]=nowy;
+            axisX[i % 6] = nowx;
+            axisY[i % 6] = nowy;
         }
         return false;
     }
